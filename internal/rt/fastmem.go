b8981adecfbd21b85acacea55ec9d7a193afae43
wudongyang.2022 <wudongyang.2022@bytedance.com>
2025-02-07 17:47:55 +0800

    feat: 安全修改地址

--- internal/rt/fastmem.go
+++ internal/rt/fastmem.go
@@ -108,11 +108,13 @@ func GuardSlice(buf *[]byte, n int) {
 }
 
 func AddPtr(a unsafe.Pointer, b uintptr) unsafe.Pointer {
+	//return unsafe.Pointer(uintptr(a) + b)
 	return unsafe.Add(a, b)
 }
 
 func SubPtr(a unsafe.Pointer, b uintptr) unsafe.Pointer {
-	return unsafe.Pointer(uintptr(a) - b)
+	//return unsafe.Pointer(uintptr(a) - b)
+	return unsafe.Add(a, -b)
 }
 
 func PtrOffset(a unsafe.Pointer, b unsafe.Pointer) int {
